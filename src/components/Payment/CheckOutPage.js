7e3cbca5938a0799bbac1af9e65daacefdc49504
shafayatC <shafayatplay@gmail.com>
2023-04-04 12:28:51 +0600

    checkout

---
+++ src/components/Payment/CheckOutPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css'; 
+
+const CheckOutPage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CheckOutPage;
