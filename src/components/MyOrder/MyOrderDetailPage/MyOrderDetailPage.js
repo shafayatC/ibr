1de21cc4bd06f11eebab3297575af6cd0dc6120e
shafayatC <shafayatplay@gmail.com>
2023-04-10 19:46:28 +0600

    check image url

--- src/components/MyOrder/MyOrderDetailPage/MyOrderDetailPage.js
+++ src/components/MyOrder/MyOrderDetailPage/MyOrderDetailPage.js
@@ -145,7 +145,6 @@ const MyOrderDetailPage = () => {
         <>
 
             {console.log(getFilterSuggest)}
-            {/* console.log(getOrderDetailInfo) */}
             <div style={{ backgroundImage: `url(${bg})`, minHeight: 'calc(100vh - 44px)' }} >
                 <div className='container mx-auto relative'>
                     <div className="flex items-center justify-center py-3">
@@ -193,7 +192,7 @@ const MyOrderDetailPage = () => {
                         <div className='px-7'>
                             <div className={`grid sm:grid-cols-1 md:grid-cols-${getOrderDetailInfo.length > 3 ? "4" : getOrderDetailInfo.length} lg:grid-cols-${getOrderDetailInfo.length > 3 ? "4" : getOrderDetailInfo.length} gap-4 pt-2 ml-2  pr-3`}>
                                 {currentImages.map((data, index) => (
-                                    <div key={index} className={getOrderDetailInfo.length === 1 && "flex justify-center"}>
+                                    data.file_relative_path.toLowerCase().indexOf(getFilterText.toLowerCase()) > -1 &&  <div key={index} className={getOrderDetailInfo.length === 1 && "flex justify-center"}>
                                         <div className={`img-container  bg-no-repeat  cursor-pointer img-bag_2 ${getOrderDetailInfo.length === 1 ? "h-[400px] justify-center" : "img-bag"}`}
                                             onClick={() => viewImg((currentPage - 1) * itemsPerPage + index)}
                                             style={{
@@ -206,6 +205,7 @@ const MyOrderDetailPage = () => {
                         </div>
                     }
                 </div>
+
                 {showImage &&
                     <div>
                         <div
@@ -226,57 +226,9 @@ const MyOrderDetailPage = () => {
                                 <p className=" text-white text-center py-1 top-1 bg-teal-500  font-semibold">Beautify imagery with Ad-on Professional Services</p>
                                 <div className="pt-10 mx-auto ">
                                     <div className="w-[400px] h-[400px] border border-theme-shade  relative">
-                                        <CompareImage
+                                    <CompareImage
                                             bottomImage={getOrderDetailInfo[getImgIndex].default_compressed_output_public_url}
                                             topImage={getOrderDetailInfo[getImgIndex].compressed_raw_url} />
-                                        <p className="absolute top-0 right-0  bg-teal-500 text-white px-3 text-xs py-1  rounded-l-3xl z-10">{getImgIndex + 1}</p>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="absolute top-[50%] w-full" style={{ transform: 'translateY(-50%)' }}>
-                                <button disabled={getImgIndex == 0} onClick={() => { setImgIndex(getImgIndex - 1) }} className="float-left ml-36 cursor-pointer text-white disabled:text-black ">
-                                    <i className="fa-solid fa-circle-chevron-left text-4xl "></i>
-                                    {/* <i class="fa-solid fa-circle-chevron-left"></i> */}
-                                </button>
-                                <button disabled={getImgIndex == getOrderDetailInfo.length - 1} onClick={() => { setImgIndex(getImgIndex + 1) }} className="float-right mr-36 cursor-pointer text-white  disabled:text-black ">
-                                    <i className="fa-solid fa-circle-chevron-right text-4xl "></i>
-                                    {/* <i class="fa-solid fa-circle-chevron-right"></i> */}
-                                </button>
-                            </div>
-                            <div className="absolute right-4 top-4 flex gap-2">
-                                <button
-                                    onClick={handleClose}
-                                    className="bg-white w-10 h-10 border border-theme-shade rounded-full"
-                                >
-                                    <i className="fa-solid fa-xmark"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                }
-                
-                {showImage &&
-                    <div>
-                        <div
-                            style={{
-                                position: "absolute",
-                                top: 0,
-                                left: -10,
-                                right: 0,
-                                bottom: 0,
-                                zIndex: 99,
-                                display: "flex",
-                                justifyContent: "center",
-                                backgroundImage: `url(${bg})`
-                            }}
-                        >
-                            <div className="h-[550px] w-[800px] bg-white mt-5 relative rounded-md z-50 flex flex-col">
-
-                                <p className=" text-white text-center py-1 top-1 bg-teal-500  font-semibold">Beautify imagery with Ad-on Professional Services</p>
-                                <div className="pt-10 mx-auto ">
-                                    <div className="w-[400px] h-[400px] border border-theme-shade  relative">
-                                        <img className="h-full" src={getOrderDetailInfo[getImgIndex].compressed_raw_url} />
                                         <p className="absolute top-0 right-0  bg-teal-500 text-white px-3 text-xs py-1  rounded-l-3xl z-10">{getImgIndex + 1}</p>
                                     </div>
                                 </div>
